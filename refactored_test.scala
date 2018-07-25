459d46752bb11a75f4778d2b4e71961506e02ae5
High-drogen <defaultinstantmail@gmail.com>
2018-07-24 20:43:51 -0400

    use the regex pattern from the JSON file

--- refactored_test.scala
+++ refactored_test.scala
@@ -2,40 +2,21 @@ import scala.util.matching.Regex
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.types._ 
 
-// ----- THIS WILL BE CONTAINED IN AN EXTERNAL FILE -----------------------------------------------------------------------------
-
-//val PATTERN = """(\d{2}[^a-zA-Z]*)\s(I|E|W|T|D|F|S|V)\s(.*)""".r
+// -------------------------------------------------------------------------------------------------------------------------------
 
 val sqlContext = new SQLContext(sc)
-val df = sqlContext.read.option("multiline", true).json("/Users/vaati/Desktop/LogParser/LogParser/info.json")
-val child = df.select(df("ANDROID.pattern")).toDF("pattern")
+val df = sqlContext.read.option("multiline", true).json("/Users/vaati/Desktop/LogParser/LogParser/info.json").toDF()
 
+val child_pattern = df.select("ANDROID.pattern")
 
-println(Console.RED + child.getClass + Console.WHITE)
+child_pattern.registerTempTable("tempcust")
 
+val firstCityState = sqlContext.sql("SELECT * FROM tempcust").first()
 
-val thePattern = child.select(child("pattern"))
-thePattern.show()
-val DFtoProcess = sqlContext.sql("SELECT * FROM pattern")
-//val str: String = thePattern.mkString()
-
-
-
-//val regexStr: String = child.select(child("pattern")).toString()
-
-//println("CLASS OF REGEX " + regexStr.getClass)
-//println( Console.RED + "STRING CONTENT: " + regexStr + Console.WHITE )
-
-//val strrr = "Something"
-//println( Console.RED + strrr + Console.WHITE )
-//println( Console.RED + strrr.getClass + Console.WHITE )
-
-//val PATTERN = new Regex(regexStr)
-
-//println( Console.RED + "STRING CONTENT: " + PATTERN + Console.WHITE )
+val PATTERN = firstCityState.getString(0).r
 
 // ------------------------------------------------------------------------------------------------------------------------------
-/*
+
 case class LogRecord(system: String, timestamp: String, SeverityLevel: String, message: String)
 
 def parseLogLine(pattern: Regex): ( String => LogRecord) = {
@@ -55,27 +36,9 @@ def parseLogLine(pattern: Regex): ( String => LogRecord) = {
 }
 
 val logData = sc.textFile("/Users/vaati/Desktop/loghub/Andriod/Andriod_2k.log")
-//val accessLogs = sc.textFile("/Users/vaati/Desktop/loghub/Hadoop/Hadoop_2k.log")
-//val logData = sc.textFile("/Users/vaati/Desktop/loghub/Apache/Apache_2k.log")
-//val logData = sc.textFile("/Users/vaati/Desktop/loghub/BGL/BGL_2k.log")
-//val logData = sc.textFile("/Users/vaati/Desktop/loghub/HDFS/HDFS_2k.log")
-//val logData = sc.textFile("/Users/vaati/Desktop/loghub/OpenStack/OpenStack_2k.log")
-//val logData = sc.textFile("/Users/vaati/Desktop/loghub/Spark/Spark_2k.log")
-//val logData = sc.textFile("/Users/vaati/Desktop/loghub/Windows/Windows_2k.log")
-//val logData = sc.textFile("/Users/vaati/Desktop/loghub/Zookeeper/Zookeeper_2k.log")
-// val logData = sc.textFile(LOG_PATH) // TODO: get it from JSON file
 
 def accessLogs = logData.map( parseLogLine(PATTERN) ).toDF()
 
 accessLogs.show()
-*/
-//accessLogs.write.format("com.databricks.spark.csv").option("delimiter",";").save("/Users/vaati/Desktop/loghub/Andriod/Android.csv")
-//accessLogs.write.format("com.databricks.spark.csv").option("delimiter",";").save("/Users/vaati/Desktop/loghub/Hadoop/Hadoop.csv")
-//accessLogs.write.format("com.databricks.spark.csv").option("delimiter",";").save("/Users/vaati/Desktop/loghub/Apache/Apache.csv")
-//accessLogs.write.format("com.databricks.spark.csv").option("delimiter",";").save("/Users/vaati/Desktop/loghub/BGL/BGL.csv")
-//accessLogs.write.format("com.databricks.spark.csv").option("delimiter",";").save("/Users/vaati/Desktop/loghub/HDFS/HDFS.csv")
-//accessLogs.write.format("com.databricks.spark.csv").option("delimiter",";").save("/Users/vaati/Desktop/loghub/OpenStack/OpenStack.csv")
-//accessLogs.write.format("com.databricks.spark.csv").option("delimiter",";").save("/Users/vaati/Desktop/loghub/Spark/Spark.csv")
-//accessLogs.write.format("com.databricks.spark.csv").option("delimiter",";").save("/Users/vaati/Desktop/loghub/Windows/Windows.csv")
-//accessLogs.write.format("com.databricks.spark.csv").option("delimiter",";").save("/Users/vaati/Desktop/loghub/Zookeeper/Zookeeper.csv")
-// accessLogs.write.format("com.databricks.spark.csv").option("delimiter",";").save(CSV_PATH) // TODO: get it from JSON file
+
+accessLogs.write.format("com.databricks.spark.csv").option("delimiter",";").save("/Users/vaati/Desktop/loghub/Andriod/Android.csv")
